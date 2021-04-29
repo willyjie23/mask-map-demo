aab6096340b0aeb57ba20ee426b955df20f687e8
willychang <a0903084107@gmail.com>
2021-04-29 22:21:07 +0800

    select options

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="row no-gutters">
+      <!-- 選擇所在區域 -->
+      <div class="toolbox col-sm-3 p-2 bg-white">
+        <div class="form-group d-flex">
+          <label for="cityName" class="col-form-label mr-2 text-right">縣市</label>
+          <div class="flex-fill">
+              <select id="cityName" class="form-control" v-model="select.city">
+              <option value="">請選擇縣市</option>
+              <option :value="c.CityName" v-for="c in cityName" :key="c.CityName">
+              {{ c.CityName }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group d-flex">
+          <label for="area" class="col-form-label mr-2 text-right">地區</label>
+          <div class="flex-fill">
+            <select id="area" class="form-control" v-model="select.area">
+              <option value="">請選擇地區</option>
+              <option :value="a.AreaName" v-for="a in cityName.find((city) => city.CityName === select.city).AreaList" :key="a.AreaName">
+                {{ a.AreaName }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <!-- 顯示藥局位置 -->
+      <div class="col-sm-9">
+        <div id="map"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import cityName from './assets/cityName.json';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    cityName,
+    // select 先暫時設定為空物件
+    select: {
+      city: '臺北市',
+      area: '中正區',
+    },
+  }),
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import 'bootstrap/scss/bootstrap';
+
+  #map {
+    position: relative;
+    height: 100vh;
+  }
 </style>
